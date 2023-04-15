c4d1057632760bb098984bc45964a4bb410c0b8c
daddygi <vlimbaga49@gmail.com>
2023-04-15 20:34:59 +0800

    "feat: user, admin page not done"

--- src/app/admin-home/page.tsx
+++ src/app/admin-home/page.tsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/Header";
+import Image from "next/image";
+import Button from "../components/LogoutButton";
 
-export default function AdminHome() {
+export default function Admin() {
   return (
-    <div>
-        <div>
-
-        </div>
-        <div>
-            
-        </div>
-    </div>
-  )
+    <main>
+      <nav>
+        <Header />
+      </nav>
+      <div className="mt-2 ml-3">
+        <Button />
+      </div>
+      {/* dashboard */}
+      <div className="flex items-center justify-between pt-40">
+        <a
+          href="#"
+          className=" ml-20 border-2 border-blue-500 text-blue-500 rounded-full px-12 py-2 inline-block font semibold hover:bg-blue-500 hover:text-white"
+        >
+          Add Inhabitants
+        </a>
+        <a
+          href="#"
+          className="border-2 border-blue-500 text-blue-500 rounded-full px-12 py-2 inline-block font semibold hover:bg-blue-500 hover:text-white"
+        >
+          Inhabitants
+        </a>
+        <a
+          href="#"
+          className="mr-20 border-2 border-blue-500 text-blue-500 rounded-full px-12 py-2 inline-block font semibold hover:bg-blue-500 hover:text-white"
+        >
+          Contact Developers
+        </a>
+      </div>
+    </main>
+  );
 }
--- src/app/user-home/page.tsx
+++ src/app/user-home/page.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/Header";
+import Image from "next/image";
+import Button from "../components/LogoutButton";
 
 export default function UserHome() {
   return (
-    <h1>user home</h1>
-  )
+    <main>
+      <nav>
+        <Header />
+      </nav>
+      <div className="mt-2 ml-3">
+        <Button />
+      </div>
+      <div className="flex flex-col items-center justify-center w-full px-20 pt-10">
+        <Image
+          className="h-auto max-w-full rounded-full"
+          src="/images/temp id.jpg"
+          alt="temp"
+          width={300}
+          height={100}
+        />
+        <p className="font-bold">Sample S. Sample</p>
+        <div>
+          <a
+            href="#"
+            className="border-2 border-blue-500 text-blue-500 rounded-full px-11 py-2 mt-5 inline-block font semibold hover:bg-blue-500 hover:text-white"
+          >
+            View Profile
+          </a>
+        </div>
+        <div>
+          <a
+            href="#"
+            className="border-2 border-blue-500 text-blue-500 rounded-full px-12 py-2 mt-2 inline-block font semibold hover:bg-blue-500 hover:text-white"
+          >
+            Edit Profile
+          </a>
+        </div>
+      </div>
+    </main>
+  );
 }
