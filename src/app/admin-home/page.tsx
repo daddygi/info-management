a35119799f835c32540df0797630b6f4983e1dab
daddygi <vlimbaga49@gmail.com>
2023-04-15 16:06:23 +0800

    "feat: LoginPage done"

---
+++ src/app/admin-home/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function AdminHome() {
+  return (
+    <div>
+        <div>
+
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
