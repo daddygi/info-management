82bdbe42c8c099b2dff0945ae6eeb1a217e19bb1
ianachiapco091 <ianachiapco092@gmail.com>
2023-04-30 14:42:10 +0800

    User Register Update

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from '../components/Header';
 
 export default function RegisterPage() {
   return (
     <main>
-      <p>Hello</p>
+      <nav><Header /></nav>
     </main>
   );
 }
--- src/app/user-home/page.tsx
+++ src/app/user-home/page.tsx
@@ -2,37 +2,39 @@ import React from "react";
 import Header from "../components/Header";
 import Image from "next/image";
 import Button from "../components/LogoutButton";
+import Link from "next/link";
 
 export default function UserHome() {
+
   return (
     <main>
       <nav>
         <Header />
       </nav>
-      <div className="mt-2 ml-3">
+      <div className="mt-2 ml-2 flex">
         <Button />
+        <p className = "absolute right-0 mr-2">Sample, Sample S.</p>
       </div>
-      <div className="flex flex-col items-center justify-center w-full px-20 pt-10">
+      <div className="flex flex-col items-center justify-center w-full">
         <Image
           className="h-auto max-w-full rounded-full"
-          src="/images/temp id.jpg"
+          src="/images/tempB.png"
           alt="temp"
-          width={300}
-          height={100}
+          width={200}
+          height={200}
         />
-        <p className="font-bold">Sample S. Sample</p>
         <div>
           <a
-            href="#"
-            className="border-2 border-blue-500 text-blue-500 rounded-full px-11 py-2 mt-5 inline-block font semibold hover:bg-blue-500 hover:text-white"
+            href = "/userviewprof"
+            className="border-2 border-blue-500 text-blue-500 rounded-full px-11 py-2 mt-2 mb-3 inline-block font semibold hover:bg-blue-500 hover:text-white"
           >
             View Profile
           </a>
         </div>
         <div>
           <a
-            href="#"
-            className="border-2 border-blue-500 text-blue-500 rounded-full px-12 py-2 mt-2 inline-block font semibold hover:bg-blue-500 hover:text-white"
+            href="/usereditprofile"
+            className="border-2 border-blue-500 text-blue-500 rounded-full px-12 py-2 mt-2 inline-block font semibold hover:bg-blue-500 hover:text-white mb-3"
           >
             Edit Profile
           </a>
